96ada2ddd2da2ff132b3a138e189c689bcf4c84e
Josh Prewer <joshua.prewer@gmail.com>
2013-07-03 17:08:39 +0100

    Fixed broken graph due to wrong object name

--- StoriesPerPersonPerSprint/StoriesPerPersonPerSprintGraph.js
+++ StoriesPerPersonPerSprint/StoriesPerPersonPerSprintGraph.js
@@ -5,18 +5,18 @@ function drawVisualization() {
     var sprintData = massageData();
 
     var sprintDataTable = new google.visualization.DataTable();
-    sprintData.addColumn('date', 'Sprint Date');
-    sprintData.addColumn('number', 'Stories per Sprint');
-    sprintData.addColumn('number', 'Stories per Person');
+    sprintDataTable.addColumn('date', 'Sprint Date');
+    sprintDataTable.addColumn('number', 'Stories per Sprint');
+    sprintDataTable.addColumn('number', 'Stories per Person');
 
     while (sprintData.length > 1) {
         var row = sprintData.pop();
-        sprintData.addRow([row.StartDate, row.StoriesInSprint, row.StoriesPerPerson]);
+        sprintDataTable.addRow([row.StartDate, row.StoriesInSprint, row.StoriesPerPerson]);
     }
 
     var annotatedtimeline = new google.visualization.AnnotatedTimeLine(
         document.getElementById('visualization'));
-    annotatedtimeline.draw(sprintData, { 'displayAnnotations': true });
+    annotatedtimeline.draw(sprintDataTable, { 'displayAnnotations': true });
 }
 
 google.setOnLoadCallback(drawVisualization);
