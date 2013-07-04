09f09bf580ffb046777dc5d092e5b22f1d142f66
Josh Prewer <joshua.prewer@gmail.com>
2013-07-04 12:17:16 +0100

    Table Working

---
+++ CycleTimeGraph/CycleTimeGraph.js
@@ -0,0 +1,22 @@
+﻿google.load('visualization', '1', { packages: ['annotatedtimeline'] });
+
+function drawVisualization() {
+
+    var cycleData = massageData();
+
+    var cycleDataTable = new google.visualization.DataTable();
+    cycleDataTable.addColumn('date', 'Accepted Date');
+    cycleDataTable.addColumn('number', 'Cycle Time');
+    
+
+    while (cycleData.length > 1) {
+        var row = cycleData.pop();
+        cycleDataTable.addRow([row.AcceptedDate, row.CycleTime]);
+    }
+
+    var annotatedtimeline = new google.visualization.AnnotatedTimeLine(
+        document.getElementById('visualization'));
+    annotatedtimeline.draw(cycleDataTable, { 'displayAnnotations': true });
+}
+
+google.setOnLoadCallback(drawVisualization);
